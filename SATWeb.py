9061c5a653f65b4173263792e96930d51bdb62c0
Jonathan López <jlopez@fipasoft.com.mx>
2015-02-28 18:03:19 -0600

    NEW: Método para logueo con CIEC

--- SATWeb.py
+++ SATWeb.py
@@ -10,3 +10,25 @@ class SATWeb:
     def __entrarAlaPaginaInicio(self):
         url = 'https://cfdiau.sat.gob.mx/nidp/app/login?id=SATUPCFDiCon&sid=0&option=credential&sid=0'
         self.sesion.post(url)
+
+    def logueoDeUsuarioConCIEC(self):
+        url = 'https://cfdiau.sat.gob.mx/nidp/app/login?sid=0&sid=0'
+        headers = {
+            'Accept':' text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Encoding':'gzip, deflate',
+            'Accept-Language':'en-US,en;q=0.5',
+            'Connection':'keep-alive',
+            'Host':'cfdiau.sat.gob.mx',
+            'Referer':'https://cfdiau.sat.gob.mx/nidp/app/login?id=SATUPCFDiCon&sid=0&option=credential&sid=0',
+            'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0',
+            'Content-Type':'application/x-www-form-urlencoded',
+        }
+        values = {'option':'credential', 'Ecom_User_ID':self.rfc, 'Ecom_Password':self.contrasena, 'submit':'Enviar'}
+        r = self.sesion.post(url, data=values,headers=headers)
+
+        url = 'https://portalcfdi.facturaelectronica.sat.gob.mx/'
+        r = self.sesion.get(url)
+        html = r.text
+        print(html)
+
+
