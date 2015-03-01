620e8ed984ca5c315d693073dcbe6c006f59ff8e
Jonathan López <jlopez@fipasoft.com.mx>
2015-02-28 18:53:44 -0600

    FIX: Cambiar nombres de variables a español

--- SATWeb.py
+++ SATWeb.py
@@ -13,7 +13,7 @@ class SATWeb:
 
     def __enviarFormularioConCIEC(self):
         url = 'https://cfdiau.sat.gob.mx/nidp/app/login?sid=0&sid=0'
-        headers = {
+        encabezados = {
             'Accept':' text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Encoding':'gzip, deflate',
             'Accept-Language':'en-US,en;q=0.5',
@@ -23,24 +23,28 @@ class SATWeb:
             'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0',
             'Content-Type':'application/x-www-form-urlencoded',
         }
-        values = {'option':'credential', 'Ecom_User_ID':self.rfc, 'Ecom_Password':self.contrasena, 'submit':'Enviar'}
-        self.sesion.post(url, data=values,headers=headers)
+        valores = {'option':'credential', 'Ecom_User_ID':self.rfc, 'Ecom_Password':self.contrasena, 'submit':'Enviar'}
+        self.sesion.post(url, data=valores,headers=encabezados)
 
     def __leerFormularioDeRespuesta(self):
         url = 'https://portalcfdi.facturaelectronica.sat.gob.mx/'
-        respond = self.sesion.get(url)
-        htmlSource = respond.text
-        htmlForm = HTMLForm(htmlSource, 'form')
-        inputValues = htmlForm.readAndGetInputValues()
-        return inputValues
+        respuesta = self.sesion.get(url)
+        htmlFuente = respuesta.text
+        htmlFormulario = HTMLForm(htmlFuente, 'form')
+        inputValores = htmlFormulario.readAndGetInputValues()
+        return inputValores
 
     def __leerFormularioDeAccessControl(self, valores):
         url = 'https://cfdicontribuyentes.accesscontrol.windows.net/v2/wsfederation'
-        respond = self.sesion.post(url, data=valores)
-        htmlSource = respond.text
+        respuesta = self.sesion.post(url, data=valores)
+        htmlFuente = respuesta.text
+        htmlFormulario = HTMLForm(htmlFuente, 'form')
+        inputValores = htmlFormulario.readAndGetInputValues()
+        return inputValores
 
 
     def logueoDeUsuarioConCIEC(self):
         self. __entrarAlaPaginaInicio()
         self.__enviarFormularioConCIEC()
-        inputValues = self.__leerFormularioDeRespuesta()
+        inputValores = self.__leerFormularioDeRespuesta()
+        self.__leerFormularioDeAccessControl(inputValores)
