17c9fe12dc30ea99133de321ea37762334922ee9
Jonathan López <jlopez@fipasoft.com.mx>
2015-02-28 18:02:21 -0600

    NEW: Método para entrar a la página de inicio del SAT

--- SATWeb.py
+++ SATWeb.py
@@ -2,6 +2,11 @@ import requests
 import lxml.html
 
 class SATWeb:
-    def __init__(self):
-        pass
+    def __init__(self, rfc, contrasena):
+        self.rfc = rfc
+        self.contrasena = contrasena
+        self.sesion = s = requests.Session()
 
+    def __entrarAlaPaginaInicio(self):
+        url = 'https://cfdiau.sat.gob.mx/nidp/app/login?id=SATUPCFDiCon&sid=0&option=credential&sid=0'
+        self.sesion.post(url)
