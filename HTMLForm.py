2b72a370f1e88c838cd42fdc2c2c594cf0d4871d
Jonathan López <jlopez@fipasoft.com.mx>
2015-02-28 18:41:45 -0600

    NEW: Método para leer y obtener los valores de un formulario

--- HTMLForm.py
+++ HTMLForm.py
@@ -5,3 +5,9 @@ class HTMLForm:
         self.xpathForm = xpathForm
         self.htmlSource = htmlSource
 
+    def readAndGetInputValues(self):
+        document=lxml.html.fromstring(self.htmlSource)
+        inputValues = {}
+        for input in document.xpath("//"+self.xpathForm+"/input"):
+            inputValues[input.name] = input.value
+        return inputValues;
