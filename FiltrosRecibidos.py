0301035acd749a771d3d9e91cf91ad6b1cf52119
Jonathan López <jlopez@fipasoft.com.mx>
2015-03-11 22:09:21 -0600

    NEW: Clase para el manejo de los filtros

---
+++ FiltrosRecibidos.py
@@ -0,0 +1,31 @@
+class FiltrosRecibidos:
+
+    def __init__(self):
+        self.annio=''
+        self.mes=''
+        self.dia=''
+
+    def obtenerPOST(self):
+        post={}
+        post['__ASYNCPOST'] = 'true'
+        post['__EVENTARGUMENT'] = ''
+        post['__EVENTTARGET'] = ''
+        post['__LASTFOCUS'] = ''
+        post['ctl00$MainContent$BtnBusqueda'] = 'Buscar CFDI'
+        post['ctl00$MainContent$CldFecha$DdlAnio'] = self.annio
+        post['ctl00$MainContent$CldFecha$DdlDia'] = self.dia
+        post['ctl00$MainContent$CldFecha$DdlHora'] = '0'
+        post['ctl00$MainContent$CldFecha$DdlHoraFin'] = '23'
+        post['ctl00$MainContent$CldFecha$DdlMes'] = self.mes
+        post['ctl00$MainContent$CldFecha$DdlMinuto'] = '0'
+        post['ctl00$MainContent$CldFecha$DdlMinutoFin'] = '59'
+        post['ctl00$MainContent$CldFecha$DdlSegundo'] = '0'
+        post['ctl00$MainContent$CldFecha$DdlSegundoFin'] = '59'
+        post['ctl00$MainContent$DdlEstadoComprobante'] = '-1'
+        post['ctl00$MainContent$FiltroCentral'] = 'RdoFechas'
+        post['ctl00$MainContent$TxtRfcReceptor'] = ''
+        post['ctl00$MainContent$TxtUUID'] = ''
+        post['ctl00$MainContent$ddlComplementos'] = '-1'
+        post['ctl00$MainContent$hfInicialBool'] = 'false'
+        post['ctl00$ScriptManager1'] = 'ctl00$MainContent$UpnlBusqueda|ctl00$MainContent$BtnBusqueda'
+        return post
