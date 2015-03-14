8fe464f82971b5df926baef57689c605d3eaaccf
Jonathan López <jlopez@fipasoft.com.mx>
2015-03-14 15:58:10 -0600

    NEW: Atributos privados de la clase DescargarXML

--- DescargarXML.py
+++ DescargarXML.py
@@ -3,28 +3,28 @@ import lxml.html
 
 class DescargarXML:
     def __init__(self, sesion, htmlSource, direccionDescarga):
-        self.sesion=sesion
-        self.htmlSource=htmlSource
-        self.direccionDescarga=direccionDescarga
+        self.__sesion=sesion
+        self.__htmlSource=htmlSource
+        self.__direccionDescarga=direccionDescarga
         self.__listaXML = []
 
     def obtenerEnlacesYDescargar(self):
         i=1
-        document=lxml.html.fromstring(self.htmlSource)
+        document=lxml.html.fromstring(self.__htmlSource)
         for img in document.xpath('//img[@class="BtnDescarga"]'):
             urlXML=img.attrib['onclick'].replace("return AccionCfdi('", "https://portalcfdi.facturaelectronica.sat.gob.mx/");
             urlXML=urlXML.replace("','Recuperacion');", "")
             nombre = str(i)+'.xml'
             self.__descargarXML(urlXML, nombre)
             i+=1
-            self.__listaXML.append(urlXML + nombre)
+            self.__listaXML.append(self__direccionDescarga + nombre)
 
     def obtenerListaDeDocumentosDescargados(self):
         return self.__listaXML
 
     def __descargarXML(self, urlXML, name):
-        with open(self.direccionDescarga + name , 'wb') as handle:
-            response = self.sesion.get(urlXML, stream=True)
+        with open(self.__direccionDescarga + name , 'wb') as handle:
+            response = self.__sesion.get(urlXML, stream=True)
             if not response.ok:
                 pass
 
