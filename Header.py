d2db6d68d9ad5ec5f9655ed6b23cf3447d969e45
Jonathan López <jlopez@fipasoft.com.mx>
2015-03-14 11:55:29 -0600

    NEW: Clase para el manejo de los encabezados de las peticiones

---
+++ Header.py
@@ -0,0 +1,15 @@
+class Header:
+
+    def obtener(self, referer):
+        encabezado = {
+            'Accept':' text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Encoding':'gzip, deflate',
+            'Accept-Language':'en-US,en;q=0.5',
+            'Connection':'keep-alive',
+            'Host':'cfdiau.sat.gob.mx',
+            'Referer':referer,
+            'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0',
+            'Content-Type':'application/x-www-form-urlencoded',
+        }
+        return encabezado
+
