ef3b479e53d23c2ea39d73789ad0fd40067aac16
Jonathan López <jlopez@fipasoft.com.mx>
2015-03-14 11:55:51 -0600

    FIX: Obtener el encabezado de la clase Header en el método __enviarFormularioConCIEC

--- PortalCfdi.py
+++ PortalCfdi.py
@@ -2,6 +2,7 @@ import requests
 from HTMLForm import HTMLForm
 from DescargarXML import DescargarXML
 from ParserFormatSAT import ParserFormatSAT
+from Header import Header
 
 class PortalCfdi:
     def __init__(self, rfc, contrasena):
@@ -9,23 +10,20 @@ class PortalCfdi:
         self.contrasena = contrasena
         self.sesion = requests.Session()
         self.directorioAGuardar=''
+        self.header = Header()
+        self.urlCfdiau = 'https://cfdiau.sat.gob.mx/'
 
     def __entrarAlaPaginaInicio(self):
-        url = 'https://cfdiau.sat.gob.mx/nidp/app/login?id=SATUPCFDiCon&sid=0&option=credential&sid=0'
+        url = self.urlCfdiau + '/nidp/app/login?id=SATUPCFDiCon&sid=0&option=credential&sid=0'
         self.sesion.post(url)
 
     def __enviarFormularioConCIEC(self):
-        url = 'https://cfdiau.sat.gob.mx/nidp/app/login?sid=0&sid=0'
-        encabezados = {
-            'Accept':' text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Encoding':'gzip, deflate',
-            'Accept-Language':'en-US,en;q=0.5',
-            'Connection':'keep-alive',
-            'Host':'cfdiau.sat.gob.mx',
-            'Referer':'https://cfdiau.sat.gob.mx/nidp/app/login?id=SATUPCFDiCon&sid=0&option=credential&sid=0',
-            'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0',
-            'Content-Type':'application/x-www-form-urlencoded',
-        }
+        url = self.urlCfdiau + 'nidp/app/login?sid=0&sid=0'
+        encabezados = self.header.obtener(
+            self.urlCfdiau +
+            '/nidp/app/login?id=SATUPCFDiCon&sid=0&option=credential&sid=0'
+        )
+
         valores = {'option':'credential', 'Ecom_User_ID':self.rfc, 'Ecom_Password':self.contrasena, 'submit':'Enviar'}
         self.sesion.post(url, data=valores,headers=encabezados)
 
