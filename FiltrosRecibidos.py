4f1983421115030d8820fe14611782260c3acd88
Jonathan López <jlopez@fipasoft.com.mx>
2015-03-14 11:23:39 -0600

    NEW: Método para obtener el formulario a enviar al solicitar los filtros de fecha

--- FiltrosRecibidos.py
+++ FiltrosRecibidos.py
@@ -29,3 +29,28 @@ class FiltrosRecibidos:
         post['ctl00$MainContent$hfInicialBool'] = 'false'
         post['ctl00$ScriptManager1'] = 'ctl00$MainContent$UpnlBusqueda|ctl00$MainContent$BtnBusqueda'
         return post
+
+    def obtenerPOSTFormularioFechas(self):
+        post={}
+        post['__ASYNCPOST'] = 'true'
+        post['__EVENTARGUMENT'] = ''
+        post['__EVENTTARGET']='ctl00$MainContent$RdoFechas'
+        post['__LASTFOCUS'] = ''
+        post['ctl00$MainContent$CldFecha$DdlAnio'] = '2015'
+        post['ctl00$MainContent$CldFecha$DdlDia'] = '01'
+        post['ctl00$MainContent$CldFecha$DdlHora'] = '0'
+        post['ctl00$MainContent$CldFecha$DdlHoraFin'] = '23'
+        post['ctl00$MainContent$CldFecha$DdlMes'] = '1'
+        post['ctl00$MainContent$CldFecha$DdlMinuto'] = '0'
+        post['ctl00$MainContent$CldFecha$DdlMinutoFin'] = '59'
+        post['ctl00$MainContent$CldFecha$DdlSegundo'] = '0'
+        post['ctl00$MainContent$CldFecha$DdlSegundoFin'] = '59'
+        post['ctl00$MainContent$DdlEstadoComprobante'] = '-1'
+        post['ctl00$MainContent$FiltroCentral'] = 'RdoFechas'
+        post['ctl00$MainContent$TxtRfcReceptor'] = ''
+        post['ctl00$MainContent$TxtUUID'] = ''
+        post['ctl00$MainContent$ddlComplementos'] = '-1'
+        post['ctl00$MainContent$hfInicialBool']='true'
+        post['ctl00$ScriptManager1'] = 'ctl00$MainContent$UpnlBusqueda|ctl00$MainContent$RdoFechas'
+        return post
+
