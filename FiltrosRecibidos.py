14b90f7fa844f1f03d8c8a260ae102e11604461f
Jonathan López <jlopez@fipasoft.com.mx>
2015-03-28 14:19:15 -0600

    PEP8: Cambiar nombre de variables a minusculas_

--- FiltrosRecibidos.py
+++ FiltrosRecibidos.py
@@ -7,16 +7,16 @@ class FiltrosRecibidos:
         self.annio = str(datetime.date.today().year)
         self.mes = '1'
         self.dia = '0'
-        self.horaInicial = '0'
-        self.minutoInicial = '0'
-        self.segundoInicial = '0'
-        self.horaFinal = '23'
-        self.minutoFinal = '59'
-        self.segundoFinal = '59'
-        self.rfcEmisor = ''
+        self.hora_inicial = '0'
+        self.minuto_inicial = '0'
+        self.segundo_inicial = '0'
+        self.hora_final = '23'
+        self.minuto_final = '59'
+        self.segundo_final = '59'
+        self.rfc_emisor = ''
         self.__estado = '1'  # 1.-Vigente,0.-Cancelado
         self.tipo = '-1'
-        self.folioFiscal = ''
+        self.folio_fiscal = ''
 
     def __formatea_dia(self):
         if int(self.dia) < 10:
@@ -32,17 +32,17 @@ class FiltrosRecibidos:
         post['ctl00$MainContent$BtnBusqueda'] = 'Buscar CFDI'
         post['ctl00$MainContent$CldFecha$DdlAnio'] = self.annio
         post['ctl00$MainContent$CldFecha$DdlDia'] = self.__formatea_dia()
-        post['ctl00$MainContent$CldFecha$DdlHora'] = self.horaInicial
-        post['ctl00$MainContent$CldFecha$DdlHoraFin'] = self.horaFinal
+        post['ctl00$MainContent$CldFecha$DdlHora'] = self.hora_inicial
+        post['ctl00$MainContent$CldFecha$DdlHoraFin'] = self.hora_final
         post['ctl00$MainContent$CldFecha$DdlMes'] = self.mes
-        post['ctl00$MainContent$CldFecha$DdlMinuto'] = self.minutoInicial
-        post['ctl00$MainContent$CldFecha$DdlMinutoFin'] = self.minutoFinal
-        post['ctl00$MainContent$CldFecha$DdlSegundo'] = self.segundoInicial
-        post['ctl00$MainContent$CldFecha$DdlSegundoFin'] = self.segundoFinal
+        post['ctl00$MainContent$CldFecha$DdlMinuto'] = self.minuto_inicial
+        post['ctl00$MainContent$CldFecha$DdlMinutoFin'] = self.minuto_final
+        post['ctl00$MainContent$CldFecha$DdlSegundo'] = self.segundo_inicial
+        post['ctl00$MainContent$CldFecha$DdlSegundoFin'] = self.segundo_final
         post['ctl00$MainContent$DdlEstadoComprobante'] = self.__estado
         post['ctl00$MainContent$FiltroCentral'] = self.__obten_filtro_central()
-        post['ctl00$MainContent$TxtRfcReceptor'] = self.rfcEmisor
-        post['ctl00$MainContent$TxtUUID'] = self.folioFiscal
+        post['ctl00$MainContent$TxtRfcReceptor'] = self.rfc_emisor
+        post['ctl00$MainContent$TxtUUID'] = self.folio_fiscal
         post['ctl00$MainContent$ddlComplementos'] = self.tipo
         post['ctl00$MainContent$hfInicialBool'] = 'false'
         post['ctl00$ScriptManager1'] = 'ctl00$MainContent$UpnlBusqueda|'\
