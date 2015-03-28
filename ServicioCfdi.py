0a2cb217cd07948ab17bb3cdcc2df14b9045a08c
Jonathan López <jlopez@fipasoft.com.mx>
2015-03-28 13:00:58 -0600

    PEP8: Cambiar nombre método __peticionPortalCfdi por __peticion_portal_cfdi

--- ServicioCfdi.py
+++ ServicioCfdi.py
@@ -10,7 +10,7 @@ class ServicioCfdi:
         self.__mensajeError = ''
         self.__listaDocumentosDescargados = []
 
-    def __peticionPortalCfdi(self, directorioAGuardar, filtros):
+    def __peticion_portal_cfdi(self, directorioAGuardar, filtros):
         portalCfdi = PortalCfdi(self.__rfc, self.__contrasena)
         self.__resultado = portalCfdi.consultar(directorioAGuardar, filtros)
         if not self.__resultado:
@@ -31,18 +31,18 @@ class ServicioCfdi:
         filtros.annio = annio
         filtros.mes = mes
         filtros.dia = dia
-        return self.__peticionPortalCfdi(directorioAGuardar, filtros)
+        return self.__peticion_portal_cfdi(directorioAGuardar, filtros)
 
     def descargarPorAnnioYMes(self, directorioAGuardar, annio, mes):
         filtros = FiltrosRecibidos()
         filtros.annio = annio
         filtros.mes = mes
-        return self.__peticionPortalCfdi(directorioAGuardar, filtros)
+        return self.__peticion_portal_cfdi(directorioAGuardar, filtros)
 
     def descargarPorFolioFiscal(self, directorioAGuardar, folioFiscal):
         filtros = FiltrosRecibidos()
         filtros.folioFiscal = folioFiscal
-        return self.__peticionPortalCfdi(directorioAGuardar, filtros)
+        return self.__peticion_portal_cfdi(directorioAGuardar, filtros)
 
     def descargarPorFiltros(self, directorioAGuardar, filtros):
         return self.__peticionPortalCfdi(directorioAGuardar, filtros)
