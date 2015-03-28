a3d6013909771052daefed312e1f6c07f6a696f1
Jonathan López <jlopez@fipasoft.com.mx>
2015-03-28 11:44:45 -0600

    PEP8: E225 missing whitespace around operator

--- DescargarXML.py
+++ DescargarXML.py
@@ -5,23 +5,23 @@ import lxml.html
 class DescargarXML:
 
     def __init__(self, sesion, htmlSource, direccionDescarga):
-        self.__sesion=sesion
-        self.__htmlSource=htmlSource
-        self.__direccionDescarga=direccionDescarga
+        self.__sesion = sesion
+        self.__htmlSource = htmlSource
+        self.__direccionDescarga = direccionDescarga
         self.__listaXML = []
 
     def obtenerEnlacesYDescargar(self, nombreDefault=''):
-        i=1
-        document=lxml.html.fromstring(self.__htmlSource)
+        i = 1
+        document = lxml.html.fromstring(self.__htmlSource)
         for img in document.xpath('//img[@class="BtnDescarga"]'):
-            urlXML=img.attrib['onclick'].replace("return AccionCfdi('", "https://portalcfdi.facturaelectronica.sat.gob.mx/");
-            urlXML=urlXML.replace("','Recuperacion');", "")
-            if nombreDefault !='' :
+            urlXML = img.attrib['onclick'].replace("return AccionCfdi('", "https://portalcfdi.facturaelectronica.sat.gob.mx/");
+            urlXML = urlXML.replace("','Recuperacion');", "")
+            if nombreDefault != '' :
                 nombre = nombreDefault+'.xml'
             else:
-                nombre = str(i)+'.xml'
+                nombre = str(i) + '.xml'
             self.__descargarXML(urlXML, nombre)
-            i+=1
+            i += 1
             self.__listaXML.append(self.__direccionDescarga + nombre)
 
     def obtenerListaDeDocumentosDescargados(self):
