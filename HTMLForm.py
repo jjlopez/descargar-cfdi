6520eacf8b59c463eefeef0b43515165d8106bc4
Jonathan López <jlopez@fipasoft.com.mx>
2015-03-28 14:15:27 -0600

    PEP8: Cambiar nombre de variables a minusculas_

--- HTMLForm.py
+++ HTMLForm.py
@@ -3,15 +3,15 @@ import lxml.html
 
 class HTMLForm:
 
-    def __init__(self, htmlSource, xpathForm):
-        self.xpathForm = xpathForm
-        self.htmlSource = htmlSource
+    def __init__(self, html_source, xpath_form):
+        self.xpath_form = xpath_form
+        self.html_source = html_source
 
     def get_form_values(self):
-        inputValues = self.read_input_values()
-        selectValues = self.read_select_values()
-        values = inputValues.copy()
-        values.update(selectValues)
+        input_values = self.read_input_values()
+        select_values = self.read_select_values()
+        values = input_values.copy()
+        values.update(select_values)
         return values
 
     def read_input_values(self):
@@ -21,8 +21,8 @@ class HTMLForm:
         return self.read_and_get_values("select")
 
     def read_and_get_values(self, element):
-        document = lxml.html.fromstring(self.htmlSource)
-        inputValues = {}
-        for input in document.xpath("//"+self.xpathForm+"/"+element):
-            inputValues[input.name] = input.value
-        return inputValues
+        document = lxml.html.fromstring(self.html_source)
+        input_values = {}
+        for input in document.xpath("//"+self.xpath_form+"/"+element):
+            input_values[input.name] = input.value
+        return input_values
