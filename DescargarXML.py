9dbcea78b807893a6ad31a1566e4e6d21bab47f3
Jonathan López <jlopez@fipasoft.com.mx>
2015-03-28 14:12:24 -0600

    PEP8: Variables con _

--- DescargarXML.py
+++ DescargarXML.py
@@ -4,35 +4,35 @@ import lxml.html
 
 class DescargarXML:
 
-    def __init__(self, sesion, htmlSource, direccionDescarga):
+    def __init__(self, sesion, html_source, direccion_descarga):
         self.__sesion = sesion
-        self.__htmlSource = htmlSource
-        self.__direccionDescarga = direccionDescarga
-        self.__listaXML = []
+        self.__html_source = html_source
+        self.__direccion_descarga = direccion_descarga
+        self.__lista_xml = []
 
-    def obtener_enlaces_descargar(self, nombreDefault=''):
+    def obtener_enlaces_descargar(self, nombre_default=''):
         i = 1
-        document = lxml.html.fromstring(self.__htmlSource)
+        document = lxml.html.fromstring(self.__html_source)
         for img in document.xpath('//img[@class="BtnDescarga"]'):
-            urlXML = img.attrib['onclick'].replace(
+            url_xml = img.attrib['onclick'].replace(
                 "return AccionCfdi('",
                 "https://portalcfdi.facturaelectronica.sat.gob.mx/"
             )
-            urlXML = urlXML.replace("','Recuperacion');", "")
-            if nombreDefault != '':
-                nombre = nombreDefault+'.xml'
+            url_xml = url_xml.replace("','Recuperacion');", "")
+            if nombre_default != '':
+                nombre = nombre_default+'.xml'
             else:
                 nombre = str(i) + '.xml'
-            self.__descargar_xml(urlXML, nombre)
+            self.__descargar_xml(url_xml, nombre)
             i += 1
-            self.__listaXML.append(self.__direccionDescarga + nombre)
+            self.__lista_xml.append(self.__direccion_descarga + nombre)
 
     def obtener_lista_documentos_descargados(self):
-        return self.__listaXML
+        return self.__lista_xml
 
-    def __descargar_xml(self, urlXML, name):
-        with open(self.__direccionDescarga + name, 'wb') as handle:
-            response = self.__sesion.get(urlXML, stream=True)
+    def __descargar_xml(self, url_xml, nombre):
+        with open(self.__direccion_descarga + nombre, 'wb') as handle:
+            response = self.__sesion.get(url_xml, stream=True)
             if not response.ok:
                 pass
 
